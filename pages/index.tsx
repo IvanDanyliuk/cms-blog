37a432ebf4a24fc1643f9a396d4b4459c913660f
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-07-25 14:11:24 +0300

    Created the Featured posts section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import { PostCard, PostWidget, Categories } from '../components';
+import { FeaturedPosts } from '../sections';
 import { getPosts } from '../services';
 import { IHome } from '../types';
 
@@ -8,10 +8,7 @@ import { IHome } from '../types';
 const Home: NextPage<IHome> = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <FeaturedPosts />
       <div className='grid grid-cols lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post, i) => <PostCard key={i} post={post.node} />)}
